44c97822303e2146e7a3c9ce8c316e78f128583a
gabrielloliveira <gabrielloliveira097@gmail.com>
2022-09-20 15:38:52 -0300

    feat: add swagger and simple jwt

--- coderockr/urls.py
+++ coderockr/urls.py
@@ -15,8 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="CodeRockr API",
+        default_version="v1",
+        description="Resolução do desafio técnico de backend",
+        terms_of_service="https://github.com/gabrielloliveira/",
+        contact=openapi.Contact(email="gabrielloliveira097@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("api/", include("coderockr.core.urls", namespace="core")),
+    path("api/", include("coderockr.accounts.urls", namespace="accounts")),
 ]
